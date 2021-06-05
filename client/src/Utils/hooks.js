d6fae9c052c20c678a7c04ad088415dd9beff05f
Matanf <matanf@yad2.co.il>
2021-06-05 20:54:11 +0300

    fixed error handling

--- client/src/Utils/hooks.js
+++ client/src/Utils/hooks.js
@@ -6,8 +6,14 @@ export const useFetchPullRequests = () => {
   const [prList, setPrList] = useState(null);
 
   const fetchPullRequests = async () => {
-    const { data = [] } = await axios.get(API_PATH);
-    setPrList(data);
+    try {
+      const { data = [] } = await axios.get(API_PATH);
+      setPrList(data);
+    } catch (error) {
+      alert(
+        `Opsss, maybe you forgot to turn on the server. The Error is: ${error.message}`
+      );
+    }
   };
 
   useEffect(() => {
